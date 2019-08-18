70dd1c4e20daff8bd786632ab60015c0fda053b6
SEGUL001 <lucassegurola@gmail.com>
2019-08-17 21:27:04 -0300

    first commit

---
+++ src/test/java/com/example/gestordecalificaciones/GestordecalificacionesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gestordecalificaciones;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestordecalificacionesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
